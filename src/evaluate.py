9ba2b036b5511dbbd72d9b5df6dddbb0ab1118a3
Simon Yu <jundayu2@illinois.edu>
2023-02-01 14:02:55 -0600

    Check statistics files

--- src/evaluate.py
+++ src/evaluate.py
@@ -18,13 +18,19 @@ def main():
     statistics_test = None
     statistics_train = None
 
-    with open(model_file_path_statistics_test, "r") as file_statistics_test:
-        statistics_test = json.load(file_statistics_test)
-        utility.plotTestingStatistics(statistics_test)
+    if os.path.isfile(model_file_path_statistics_test):
+        with open(model_file_path_statistics_test, "r") as file_statistics_test:
+            statistics_test = json.load(file_statistics_test)
+            utility.plotTestingStatistics(statistics_test)
+    else:
+        logger.log_warn("Testing statistics file missing.")
 
-    with open(model_file_path_statistics_train, "r") as file_statistics_train:
-        statistics_train = json.load(file_statistics_train)
-        utility.plotTrainingStatistics(statistics_train)
+    if os.path.isfile(model_file_path_statistics_train):
+        with open(model_file_path_statistics_train, "r") as file_statistics_train:
+            statistics_train = json.load(file_statistics_train)
+            utility.plotTrainingStatistics(statistics_train)
+    else:
+        logger.log_warn("Training statistics file missing.")
 
     return
 
