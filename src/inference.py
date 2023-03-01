ce06492eb3509c4bb3350b38904a67f5709d4ca1
Simon Yu <jundayu2@illinois.edu>
2023-03-01 15:45:29 -0600

    Run baseline alongside decomposed

--- src/inference.py
+++ src/inference.py
@@ -8,6 +8,7 @@ import network
 import sys
 import torch
 import torch.nn
+import torchvision
 import tqdm
 import utility
 import wandb
@@ -33,14 +34,23 @@ def main():
         logger.log_error("Run names missing. Quit.")
         return
 
+    device = torch.device("cuda")
+
     wandb.init(config = config.config_baseline, mode = "disabled")
+
+    accuracy_epoch_baseline = 0
+    dataset_baseline = torchvision.datasets.ImageFolder(root = wandb.config.dir_dataset)
+    model_baseline = network.BaselineNetworkA(dataset_baseline)
+    model_baseline = torch.nn.DataParallel(model_baseline)
+    model_baseline = model_baseline.to(device)
+
+    utility.loadCheckpointBest(wandb.config.dir_checkpoints, wandb.config.file_name_checkpoint_best, model_baseline)
 
     wandb.finish()
     wandb.init(config = config.config_decomposed, mode = "disabled")
 
     accuracy_epoch_decomposed = 0
     dataset_decomposed = dset.DatasetGenerated(root = wandb.config.dir_dataset)
-    device = torch.device("cuda")
     decision = deci.Decision(dataset_decomposed, device)
     model_decomposed = network.DecomposedNetworkA(dataset_decomposed)
     model_decomposed = torch.nn.DataParallel(model_decomposed)
@@ -55,8 +65,6 @@ def main():
     data_loader = torch.utils.data.DataLoader(data_loader.dataset, batch_size = wandb.config.data_loader_batch_size, shuffle = wandb.config.data_loader_shuffle, num_workers = wandb.config.data_loader_worker_count, pin_memory = True)
     progress_bar = tqdm.tqdm(total = len(data_loader), position = 0, leave = False)
 
-    wandb.finish()
-
     model_decomposed.eval()
     progress_bar.set_description_str("[INFO]: Inference progress")
 
@@ -66,13 +74,19 @@ def main():
             labels = labels.to(device, non_blocking = True)
 
             with torch.set_grad_enabled(False):
+                output_baseline = model_baseline(input)
                 outputs_decomposed = model_decomposed(input)
                 (output_decomposed, labels) = decision.make(outputs_decomposed, labels, input.size(0))
+
+                (_, predictions_baseline) = torch.max(output_baseline, 1)
                 (_, predictions_decomposed) = torch.max(output_decomposed, 1)
 
+                corrects_baseline = torch.sum(predictions_baseline == labels.data).item()
                 corrects_decomposed = torch.sum(predictions_decomposed == labels.data).item()
 
+            accuracy_batch_baseline = corrects_baseline / input.size(0)
             accuracy_batch_decomposed = corrects_decomposed / input.size(0)
+            accuracy_epoch_baseline += corrects_baseline
             accuracy_epoch_decomposed += corrects_decomposed
 
             progress_bar.n = batch_index + 1
@@ -80,8 +94,10 @@ def main():
 
     progress_bar.close()
 
+    accuracy_epoch_baseline /= len(data_loader.dataset)
     accuracy_epoch_decomposed /= len(data_loader.dataset)
 
+    logger.log_info("Testing accuracy for baseline network: " + str(accuracy_epoch_baseline) + ".")
     logger.log_info("Testing accuracy for decomposed network: " + str(accuracy_epoch_decomposed) + ".")
 
     return
