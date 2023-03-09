e67ae728cfbdecd198186e3bc550baef804c5321
Hongbo Zheng <hongboz2@illinois.edu>
2023-03-09 16:11:56 -0600

    parallelize for loop

--- src/composition.py
+++ src/composition.py
@@ -25,30 +25,72 @@ class Composition():
         for i in range (0, len(outputs_decomposed)):
             outputs_decomposed[i] = self.softmax(outputs_decomposed[i])
 
-        # Iterate through batches
-        for batch_index in range(0, batch_size):
-            output_composed_batch = []
+        class_indices_decomposed_map = []
 
-            # Iterate through original classes
-            for class_name_original in self.dataset.classes_original:
-                prediction_original = 1
+        # construct the index map
+        for class_name_original in self.dataset.classes_original:
+            prediction_original = 1
 
-                # Iterate through decomposed dataset
-                for (dataset_index, dataset) in enumerate(self.dataset.config["datasets"]):
-                    dataset_name = dataset["name"]
-                    class_name_decomposed = self.config_generate[class_name_original]["labels"][dataset_name]
+            class_indices_decomposed = []
+            for (dataset_index, dataset) in enumerate(self.dataset.config["datasets"]):
+                dataset_name = dataset["name"]
+                class_name_decomposed = self.config_generate[class_name_original]["labels"][dataset_name]
 
-                    if class_name_decomposed == "":
-                        class_name_decomposed = dataset_name + header.config_decomposed["dataset_delimiter_label"] + header.config_decomposed["dataset_label_undefined_keyword"]
+                if class_name_decomposed == "":
+                    class_name_decomposed = dataset_name + header.config_decomposed["dataset_delimiter_label"] + header.config_decomposed["dataset_label_undefined_keyword"]
 
-                    class_index_decomposed = self.dataset.class_to_idx[dataset_index][class_name_decomposed]
-                    prediction_original *= outputs_decomposed[dataset_index][batch_index][class_index_decomposed].item()
+                class_indices_decomposed.append(self.dataset.class_to_idx[dataset_index][class_name_decomposed])
 
-                output_composed_batch.append(prediction_original)
+            class_indices_decomposed_map.append(class_indices_decomposed)
 
-            output_composed.append(output_composed_batch)
+        class_indices_decomposed_map = torch.LongTensor(class_indices_decomposed_map)
+        class_indices_decomposed_map = class_indices_decomposed_map.to(self.device)
 
-        output_composed = torch.Tensor(output_composed)
+        output_composed = torch.Tensor()
         output_composed = output_composed.to(self.device)
+
+        # construct the output tensor 1 (401*batch_size * 4)
+        for (task_index, task_outputs) in enumerate(outputs_decomposed):
+            task_class_indices = class_indices_decomposed_map[:, task_index]
+
+            task_outputs_batch = torch.Tensor()
+            task_outputs_batch = task_outputs_batch.to(self.device)
+
+            for batch_index in range(0, batch_size):
+                task_outputs = torch.gather(outputs_decomposed[task_index][batch_index], 0, task_class_indices)
+                task_outputs_batch = torch.cat([task_outputs_batch, task_outputs], 0)
+
+            task_outputs_batch = torch.unsqueeze(task_outputs_batch, 0)
+            output_composed = torch.cat([output_composed, task_outputs_batch], 0)
+
+        output_composed = torch.transpose(output_composed, 1, 0)
+        output_composed = torch.prod(output_composed, 1)
+        output_composed = output_composed.view(batch_size, len(self.dataset.classes_original))
+
+        # # Iterate through batches
+        # for batch_index in range(0, batch_size):
+        #     output_composed_batch = []
+        #
+        #     # Iterate through original classes
+        #     for class_name_original in self.dataset.classes_original:
+        #         prediction_original = 1
+        #
+        #         # Iterate through decomposed dataset
+        #         for (dataset_index, dataset) in enumerate(self.dataset.config["datasets"]):
+        #             dataset_name = dataset["name"]
+        #             class_name_decomposed = self.config_generate[class_name_original]["labels"][dataset_name]
+        #
+        #             if class_name_decomposed == "":
+        #                 class_name_decomposed = dataset_name + header.config_decomposed["dataset_delimiter_label"] + header.config_decomposed["dataset_label_undefined_keyword"]
+        #
+        #             class_index_decomposed = self.dataset.class_to_idx[dataset_index][class_name_decomposed]
+        #             prediction_original *= outputs_decomposed[dataset_index][batch_index][class_index_decomposed].item()
+        #
+        #         output_composed_batch.append(prediction_original)
+        #
+        #     output_composed.append(output_composed_batch)
+        # 
+        # output_composed = torch.Tensor(output_composed)
+        # output_composed = output_composed.to(self.device)
 
         return output_composed
