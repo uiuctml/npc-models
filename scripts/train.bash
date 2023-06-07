ac45fd00ac503a1e810d0f7989ddc1c70e0d9976
Simon Yu <jundayu2@illinois.edu>
2023-06-07 15:19:46 -0500

    Add training bash script

---
+++ scripts/train.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd ../src
+
+./train_baseline.py -f 1 -s 84
+./train_baseline.py -f 1 -s 126
+./train_baseline.py -f 1 -s 168
+./train_baseline.py -f 1 -s 210
+
+./train_decomposed.py -f 1 -s 84
+./train_decomposed.py -f 1 -s 126
+./train_decomposed.py -f 1 -s 168
+./train_decomposed.py -f 1 -s 210
+
+./train_decomposed.py -f 0 -s 84
+./train_decomposed.py -f 0 -s 126
+./train_decomposed.py -f 0 -s 168
+./train_decomposed.py -f 0 -s 210
