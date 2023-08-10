efe02a3be431223da7079400d5a4e75afaca7e75
Lucas Zhou <yangz17@illinois.edu>
2023-08-10 17:55:32 -0500

    add ViTB32ClipMtl

--- src/network.py
+++ src/network.py
@@ -141,7 +141,6 @@ class ResNet101CLIPMTL(torch.nn.Module):
                 parameter.requires_grad = False
 
         net_fc_in_features = self.net.visual.output_dim
-        self.net.fc = torch.nn.Identity()
 
         layer_list = []
 
@@ -252,6 +251,49 @@ class ViTB32MTL(torch.nn.Module):
     def forward(self, input):
         outputs_head = []
         output_neck = self.net(input)
+
+        for head in self.net.heads_mtl:
+            head_layer = list(head.values())[0]
+            outputs_head.append(head_layer(output_neck))
+
+        return outputs_head
+
+    def getOptimizerParameters(self):
+        if header.config_decomposed["fine_tuning"]:
+            return self.net.parameters()
+        else:
+            return self.net.heads_mtl.parameters()
+
+class ViTB32CLIPMTL(torch.nn.Module):
+    def __init__(self, config_dataset, device):
+        super().__init__()
+
+        self.net = self.net = clip.load(name = "ViT-B/32", device = device)[0]
+
+        if not header.config_decomposed["fine_tuning"]:
+            for parameter in self.net.parameters():
+                parameter.requires_grad = False
+
+        net_fc_in_features = self.net.visual.output_dim
+
+        layer_list = []
+
+        for dataset_entry in config_dataset["datasets"]:
+            dataset_name = dataset_entry["name"]
+            dataset_labels = dataset_entry["labels"]
+
+            layer = torch.nn.Linear(net_fc_in_features, len(dataset_labels))
+            layer_dict = torch.nn.ModuleDict({dataset_name: layer})
+
+            layer_list.append(layer_dict)
+
+        self.net.heads_mtl = torch.nn.ModuleList(layer_list)
+
+        return
+
+    def forward(self, input):
+        outputs_head = []
+        output_neck = self.net.encode_image(input)
 
         for head in self.net.heads_mtl:
             head_layer = list(head.values())[0]
