0d7f3837e7fed12b1f30e2ad42cf40d3abf81700
Simon Yu <jundayu2@illinois.edu>
2023-08-11 15:32:01 -0500

    Add seed 42 for training

--- scripts/train.bash
+++ scripts/train.bash
@@ -2,16 +2,19 @@
 
 cd ../src
 
+./train_baseline.py -f 1 -s 42
 ./train_baseline.py -f 1 -s 84
 ./train_baseline.py -f 1 -s 126
 ./train_baseline.py -f 1 -s 168
 ./train_baseline.py -f 1 -s 210
 
+./train_decomposed.py -f 1 -s 42
 ./train_decomposed.py -f 1 -s 84
 ./train_decomposed.py -f 1 -s 126
 ./train_decomposed.py -f 1 -s 168
 ./train_decomposed.py -f 1 -s 210
 
+./train_decomposed.py -f 0 -s 42
 ./train_decomposed.py -f 0 -s 84
 ./train_decomposed.py -f 0 -s 126
 ./train_decomposed.py -f 0 -s 168
