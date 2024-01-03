923dc2c7fb48770e6661875cc84d48b4e9ffbee9
Simon Yu <jundayu2@illinois.edu>
2024-01-03 15:15:34 -0600

    Fix training script

--- scripts/train.bash
+++ scripts/train.bash
@@ -2,8 +2,8 @@
 
 cd ../src
 
-./train_baseline.py -m "resnet152_mtl" -b 384 -e 50 -c 0 -s 42
-./train_baseline.py -m "vit_b_32_mtl" -b 384 -e 50 -c 0 -s 42
+./train_baseline.py -m "resnet152" -b 384 -e 50 -c 0 -s 42
+./train_baseline.py -m "vit_b_32" -b 384 -e 50 -c 0 -s 42
 
 ./train_decomposed.py -m "resnet152_mtl" -b 384 -e 50 -c 0 -s 42
 ./train_decomposed.py -m "vit_b_32_mtl" -b 384 -e 50 -c 0 -s 42
