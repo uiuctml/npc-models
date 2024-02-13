b6439a57be2657d93a0253ebfb510cc28792d1f4
Simon Yu <jundayu2@illinois.edu>
2024-02-12 20:53:47 -0600

    Add targeted attribute header variable

--- src/header.py
+++ src/header.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import type
 
+attack_targeted_attribute = 0
 cuda_allow_tf32 = False
 dir_dataset_test_adversarial = "../../gtsrb-dataset/images/split/adversarial/test"
 file_path_dataset_config = "../../visat-dataset-tools/config/gtsrb.json"
