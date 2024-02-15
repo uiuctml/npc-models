db54fc55cff9e685a3c093865303901e91529b5d
Simon Yu <jundayu2@illinois.edu>
2024-02-15 14:23:13 -0600

    Working on composed training

---
+++ src/train_composed.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+
+import argument
+import composition
+import dataset
+import header
+import logger
+import model
+import spn
+import test_composed
+import torch
+import tqdm
+import utility
+import wandb
+
+def negativeLogLikelihood(output, label):
+    label = label.reshape(-1, 1)
+    output = torch.gather(output, 1, label)
+
+    return torch.sum(-1 * torch.log(output))
+
+def train(model_decomposed, spn_joint, spn_marginal, dataset_train, data_loader, criterion, optimizer_decomposed, optimizer_spn, device, batch_step):
+    accuracy_epoch_composed = 0
+    accuracy_epoch_list_decomposed = []
+    loss_epoch = 0
+    config_dataset = dataset_train.config
+    progress_bar = tqdm.tqdm(total = len(data_loader), position = 0, leave = False)
+    spn_output_rows = len(dataset_train.classes_original)
+    spn_output_cols = 1
+
+    for attribute in config_dataset["attributes"]:
+        attribute_labels = attribute["labels"]
+        spn_output_cols *= len(attribute_labels)
+        accuracy_epoch_list_decomposed.append(0)
+
+    model_decomposed.train()
+    progress_bar.set_description_str("[INFO]: Training progress")
+
+    with torch.set_grad_enabled(True):
+        for (batch_index, (input, labels_decomposed, labels_original, _)) in enumerate(data_loader):
+            input = input.to(device, non_blocking = True)
+            labels_decomposed = labels_decomposed.to(device, non_blocking = True)
+            labels_original = labels_original.to(device, non_blocking = True)
+
+            (outputs_decomposed, _) = model_decomposed(input)
+
+            outputs_decomposed = utility.applySoftmaxDecomposed(outputs_decomposed)
+            output_composed = composition.Composition.spn(outputs_decomposed, spn_joint, spn_marginal, spn_output_rows, spn_output_cols, device)
+
+            (_, predictions_composed) = torch.max(output_composed, 1)
+
+            loss = criterion(output_composed, labels_original)
+
+            if header.config_decomposed["use_l2_loss"]:
+                l2_norm = utility.computeL2Norm(model_decomposed.parameters())
+                loss_l2 = header.config_decomposed["l2_lambda"] * l2_norm
+                loss += loss_l2
+
+            loss.backward()
+            optimizer_decomposed.step()
+
+            spn_joint.backward()
+            # TODO Step SPN optimizer
+            # optimizer_spn.step()
+
+            # TODO Copy SPN weights from joint to marginal
+
+            corrects_composed = torch.sum(predictions_composed == labels_original.data).item()
+
+            for i in range(0, len(config_dataset["attributes"])):
+                (_, predictions_decomposed) = torch.max(outputs_decomposed[i], 1)
+
+                corrects_decomposed = torch.sum(predictions_decomposed == labels_decomposed[:, i].data).item()
+                accuracy_epoch_list_decomposed[i] += corrects_decomposed
+
+            loss_batch = loss.item()
+
+            accuracy_epoch_composed += corrects_composed
+            loss_epoch += loss_batch
+
+            progress_bar.n = batch_index + 1
+            progress_bar.refresh()
+
+    progress_bar.close()
+
+    for (i, dataset_entry) in enumerate(config_dataset["attributes"]):
+        accuracy_epoch_list_decomposed[i] /= len(data_loader.dataset)
+
+        # TODO Replace with wandb
+        logger.log_info("Decomposed training accuracy for \"" + dataset_entry["name"] + "\": " + str(accuracy_epoch_list_decomposed[i]) + ".")
+
+    accuracy_epoch_composed /= len(data_loader.dataset)
+    loss_epoch /= len(data_loader)
+
+    # TODO Replace with wandb
+    logger.log_info("Composed training accuracy: " + str(accuracy_epoch_composed) + ".")
+
+    return batch_step
+
+def main():
+    resume = argument.processArgumentsTrainComposed()
+
+    utility.setSeed(header.seed)
+    torch.backends.cuda.matmul.allow_tf32 = header.cuda_allow_tf32
+
+    if header.run_mode == "online":
+        wandb.login()
+
+    wandb.init(project = header.project_name, name = header.run_name_decomposed, config = header.config_decomposed, resume = resume, mode = header.run_mode)
+
+    utility.wAndBDefineMetrics()
+
+    logger.log_info("Started run \"" + header.run_name_decomposed + "\" and " + header.run_name_spn + ".")
+
+    accuracy_validation_best = 0
+    batch_step_test = 1
+    batch_step_train = 1
+    batch_step_validate = 1
+    criterion = negativeLogLikelihood
+    dataset_transforms = utility.createTransform(header.config_decomposed)
+    dataset_test = dataset.VISATDataset(header.config_decomposed["dir_dataset_test"], dataset_transforms)
+    dataset_train = dataset.VISATDataset(header.config_decomposed["dir_dataset_train"], dataset_transforms)
+    dataset_validation = dataset.VISATDataset(header.config_decomposed["dir_dataset_validation"], dataset_transforms)
+    config_dataset = dataset_test.config
+    data_loader_test = torch.utils.data.DataLoader(dataset_test, batch_size = header.config_decomposed["data_loader_batch_size"], shuffle = False, num_workers = header.config_decomposed["data_loader_worker_count"], pin_memory = True)
+    data_loader_train = torch.utils.data.DataLoader(dataset_train, batch_size = header.config_decomposed["data_loader_batch_size"], shuffle = header.config_decomposed["data_loader_shuffle"], num_workers = header.config_decomposed["data_loader_worker_count"], pin_memory = True)
+    data_loader_validation = torch.utils.data.DataLoader(dataset_validation, batch_size = header.config_decomposed["data_loader_batch_size"], shuffle = header.config_decomposed["data_loader_shuffle"], num_workers = header.config_decomposed["data_loader_worker_count"], pin_memory = True)
+    device = torch.device("cuda")
+    epoch = 1
+    model_decomposed = model.createModelDecomposed(device)
+    model_decomposed = torch.nn.DataParallel(model_decomposed)
+    model_decomposed = model_decomposed.to(device)
+    progress_bar = None
+    spn_joint = spn.SPN()
+    spn_marginal = spn.SPN()
+    optimizer_decomposed = torch.optim.SGD(model_decomposed.parameters(), lr = header.config_decomposed["optimizer_learning_rate"], momentum = header.config_decomposed["optimizer_momentum"], weight_decay = header.config_decomposed["optimizer_weight_decay"])
+    optimizer_spn = spn.CCCPOfflineSPNOptimizer(spn_joint, device)
+    learning_rate_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_decomposed, header.config_decomposed["learning_rate_scheduler_mode"], header.config_decomposed["learning_rate_scheduler_factor"], header.config_decomposed["learning_rate_scheduler_patience"], header.config_decomposed["learning_rate_scheduler_threshold"], header.config_decomposed["learning_rate_scheduler_threshold_mode"], header.config_decomposed["learning_rate_scheduler_cooldown"], header.config_decomposed["learning_rate_scheduler_min_learning_rate"], header.config_decomposed["learning_rate_scheduler_min_learning_rate_decay"], header.config_decomposed["learning_rate_scheduler_verbose"])
+
+    logger.log_info("Loading SPN from \"" + header.config_spn["file_path_spn"] + "\"...")
+
+    spn_joint.load(header.config_spn["file_path_spn"])
+    spn_marginal.load(header.config_spn["file_path_spn"])
+
+    logger.log_info("Loading SPN leaf node settings...")
+
+    spn_settings_joint = utility.generateSPNSettings(config_dataset, device)
+    spn_settings_marginal = torch.clone(spn_settings_joint)
+    spn_settings_marginal[:, -1] = -1
+
+    logger.log_info("Setting SPN leaf nodes...")
+
+    spn_joint.set_leaf_nodes(spn_settings_joint)
+    spn_marginal.set_leaf_nodes(spn_settings_marginal)
+
+    (accuracy_validation_best, batch_step_train, batch_step_validate, [criterion], epoch) = utility.loadCheckpoint(header.config_decomposed["dir_checkpoints"], header.config_decomposed["file_name_checkpoint"], accuracy_validation_best, batch_step_train, batch_step_validate, [criterion], epoch, [learning_rate_scheduler], model_decomposed, [optimizer_decomposed])
+    utility.loadCheckpointSPN(spn_joint, header.config_spn["dir_checkpoints"], header.config_spn["file_name_checkpoint"], resume, 0, 0, 0)
+    utility.loadCheckpointSPN(spn_marginal, header.config_spn["dir_checkpoints"], header.config_spn["file_name_checkpoint"], resume, 0, 0, 0)
+
+    if header.show_model_summary:
+        logger.log_info("Number of nodes: " + str(len(spn_joint.nodes)) + ".")
+        logger.log_info("Number of sum nodes: " + str(len(spn_joint.sum_nodes)) + ".")
+        logger.log_info("Number of product nodes: " + str(len(spn_joint.product_nodes)) + ".")
+        logger.log_info("Number of leaf nodes: " + str(len(spn_joint.leaf_nodes)) + ".")
+        logger.log_info("SPN depths: " + str(spn_joint.depth) + ".")
+        logger.log_info("SPN leaf node setting dimension: (" + str(int(spn_settings_joint.shape[0])) + ", " + str(int(spn_settings_joint.shape[1])) + ").")
+
+    if epoch <= header.config_decomposed["epochs"]:
+        progress_bar = tqdm.tqdm(total = header.config_decomposed["epochs"], position = 0)
+        progress_bar.set_description_str("[INFO]: Epoch")
+
+    while epoch <= header.config_decomposed["epochs"]:
+        if progress_bar is not None:
+            progress_bar.n = epoch
+            progress_bar.refresh()
+
+        wandb.log({"training/epoch/step": epoch})
+        wandb.log({"validation/epoch/step": epoch})
+
+        batch_step_train = train(model_decomposed, spn_joint, spn_marginal, dataset_train, data_loader_train, criterion, optimizer_decomposed, optimizer_spn, device, batch_step_train)
+        # TODO validation function
+        (accuracy_validation_epoch_list, loss_validation_epoch, batch_step_validate) = validate(model_decomposed, config_dataset, data_loader_validation, criterion, device, batch_step_validate)
+
+        learning_rate_scheduler.step(loss_validation_epoch)
+
+        accuracy_validation_epoch_mean = sum(accuracy_validation_epoch_list) / len(accuracy_validation_epoch_list)
+
+        if accuracy_validation_epoch_mean > accuracy_validation_best:
+            accuracy_validation_best = accuracy_validation_epoch_mean
+            wandb.log({"validation/epoch/accuracy_best": accuracy_validation_best})
+            utility.saveCheckpoint(header.config_decomposed["dir_checkpoints"], header.config_decomposed["file_name_checkpoint_best"], accuracy_validation_best, batch_step_train, batch_step_validate, [criterion], epoch, [learning_rate_scheduler], model_decomposed, [optimizer_decomposed])
+
+        utility.saveCheckpoint(header.config_decomposed["dir_checkpoints"], header.config_decomposed["file_name_checkpoint"], accuracy_validation_best, batch_step_train, batch_step_validate, [criterion], epoch, [learning_rate_scheduler], model_decomposed, [optimizer_decomposed])
+
+        epoch += 1
+
+    if progress_bar is not None:
+        progress_bar.close()
+
+    logger.log_info("Best validation accuracy: " + str(accuracy_validation_best) + ".")
+    wandb.summary["validation/epoch/accuracy_best"] = accuracy_validation_best
+
+    wandb.log({"testing/epoch/step": batch_step_test})
+    test_composed.test(model_decomposed, spn_joint, spn_marginal, dataset_test, data_loader_test, device, batch_step_test)
+
+    wandb.finish()
+
+    return
+
+if __name__ == "__main__":
+    main()
