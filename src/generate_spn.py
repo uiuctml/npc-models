637fd2ccaaf166d5a61535e91529253e54741541
Simon Yu <jundayu2@illinois.edu>
2024-05-13 12:34:53 -0500

    Implement manual PC generation

--- src/generate_spn.py
+++ src/generate_spn.py
@@ -2,6 +2,7 @@
 
 import header
 import json
+import os
 import torch
 import utility
 
@@ -16,13 +17,45 @@ def main():
     labels_attribute_indices = utility.getIndicesFromLabelsAttribute(labels_attribute)
     (_, label_probabilities) = utility.countAttributeJointProbabilities(config_dataset, device)
 
-    print(labels_attribute_indices)
-    print(label_probabilities)
-    print(len(label_probabilities))
+    cat_node_dict = {}
+    lines_edges = "##EDGES##\n"
+    lines_nodes = "##NODES##\n"
+    node_sequence = 0
+    node_sequence_root = node_sequence
 
-    # TODO create cat leaf nodes for all individual attributes using labels_attribute_indices
-    # TODO create product nodes using label_probabilities
-    # TODO create edges using indices
+    lines_nodes += str(node_sequence_root) + ",SUM\n"
+    node_sequence += 1
+
+    for (attribute_index, attribute) in enumerate(config_dataset["attributes"]):
+        for category_index in labels_attribute_indices[attribute["name"]].values():
+            line_cat_node = "CATNODEPRD," + str(attribute_index) + "," + str(category_index)
+            cat_node_dict[line_cat_node] = node_sequence
+            lines_nodes += str(node_sequence) + "," + line_cat_node + "\n"
+            node_sequence += 1
+
+    for probability in label_probabilities.values():
+        category_indices = probability[0]
+        frequency = probability[1]
+
+        node_sequence_prd = node_sequence
+        lines_nodes += str(node_sequence_prd) + ",PRD\n"
+        node_sequence += 1
+
+        lines_edges += str(node_sequence_root) + "," + str(node_sequence_prd) + "," + str(frequency) + "\n"
+
+        for (attribute_index, category_index) in enumerate(category_indices):
+            line_cat_node = "CATNODEPRD," + str(attribute_index) + "," + str(category_index)
+            node_sequence_cat = cat_node_dict[line_cat_node]
+
+            lines_edges += str(node_sequence_prd) + "," + str(node_sequence_cat) + "\n"
+
+    lines = lines_nodes + lines_edges
+
+    if not os.path.isdir(header.dir_output_spn):
+        os.makedirs(header.dir_output_spn, exist_ok = True)
+
+    with open(os.path.join(header.dir_output_spn, header.file_name_spn_manual), "w") as file_spn_manual:
+        file_spn_manual.writelines(lines)
 
     return
 
