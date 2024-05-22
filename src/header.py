5cfe1ec7a659f64c5cc45e8c3231be4b1d3d2217
Simon Yu <jundayu2@illinois.edu>
2024-05-22 01:41:31 -0500

    Revert "Switch to CVXPY"
    This reverts commit e09f3959e221f2ec7ef0eb330c45d48bc5f1559d.

--- src/header.py
+++ src/header.py
@@ -7,6 +7,7 @@ counterfactual_implausibility_margin_sum = 1e-6
 counterfactual_learning_rate = 1e-2
 counterfactual_moving_epsilon = 0.1
 counterfactual_qp_epsilon = 1
+counterfactual_qp_solver = "clarabel"
 counterfactual_steps = 50
 cuda_allow_tf32 = False
 dir_dataset_test_adversarial = "../../gtsrb-dataset/images/split/adversarial/test"
