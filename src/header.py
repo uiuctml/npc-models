f4d71fa2cf628276b76c2b2e0e09af6d08453406
Simon Yu <jundayu2@illinois.edu>
2024-12-04 17:13:05 -0600

    Set CBM concept loss weight to 1

--- src/header.py
+++ src/header.py
@@ -136,7 +136,7 @@ config_decomposed = {
 }
 
 config_reference = {
-    "concept_loss_weight": 0.01,
+    "concept_loss_weight": 1,
     "data_loader_batch_size": 384,
     "data_loader_shuffle": True,
     "data_loader_worker_count": multiprocessing.cpu_count(),
