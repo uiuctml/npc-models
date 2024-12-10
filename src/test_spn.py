a6dbd0fd0f1abe3f66f08421ea5cbd5a79a27961
Simon Yu <jundayu2@illinois.edu>
2024-12-10 13:01:52 -0600

    Clean up test_spn.py

--- src/test_spn.py
+++ src/test_spn.py
@@ -4,66 +4,111 @@ import argument
 import header
 import json
 import logger
+import os
 import spn
 import torch
 import utility
+import wandb
 
-def generateSPNSettings(config_dataset, device):
+def loadDataset(dir_dataset, device):
+    config_dataset = {}
+    dataset = []
+
+    with open(header.file_path_dataset_config, "r") as file_config_dataset:
+        config_dataset = json.load(file_config_dataset)
+
     labels_attribute = utility.getLabelsAttribute(config_dataset)
     labels_original = utility.getLabelsOriginal(config_dataset)
     indices_attribute = utility.getIndicesFromLabelsAttribute(labels_attribute)
     indices_original = utility.getIndicesFromLabelsOriginal(labels_original)
-    spn_settings = []
 
-    for label_original in config_dataset["mappings"].keys():
-        spn_setting = []
+    for _ in range(len(labels_attribute) + 1):
+        dataset.append([])
 
-        for attribute_name in config_dataset["mappings"][label_original]["labels"].keys():
-            label_attribute = config_dataset["mappings"][label_original]["labels"][attribute_name]
-            index_attribute = indices_attribute[attribute_name][label_attribute]
-            spn_setting.append(index_attribute)
+    if "instance_wise" not in config_dataset or config_dataset["instance_wise"] == False:
+        for class_name_original in labels_original:
+            attributes = config_dataset["mappings"][class_name_original]["labels"]
+            count_instances = len(os.listdir(os.path.join(dir_dataset, class_name_original)))
 
-        index_original = indices_original[label_original]
-        spn_setting.append(index_original)
-        spn_settings.append(spn_setting)
+            for (i, attribute_name) in enumerate(attributes.keys()):
+                category_names = attributes[attribute_name]
+                count_categories = len(indices_attribute[attribute_name])
+                binary_vector_category = torch.zeros(count_categories)
 
-    spn_settings = torch.Tensor(spn_settings)
-    spn_settings = spn_settings.to(device)
+                if isinstance(category_names, list):
+                    for category_name in category_names:
+                        index_category = indices_attribute[attribute_name][category_name]
+                        binary_vector_category[index_category] = 1
+                else:
+                    index_category = indices_attribute[attribute_name][category_names]
+                    binary_vector_category[index_category] = 1
 
-    return spn_settings
+                for _ in range(count_instances):
+                    dataset[i].append(binary_vector_category)
+
+            count_original = len(indices_original)
+            index_original = indices_original[class_name_original]
+            binary_vector_original = torch.zeros(count_original)
+            binary_vector_original[index_original] = 1
+
+            for _ in range(count_instances):
+                dataset[-1].append(binary_vector_original)
+    else:
+        for image_name in config_dataset["mappings"]:
+            class_name_original = image_name.split('/')[0]
+            attributes = config_dataset["mappings"][image_name]["labels"]
+
+            for (i, attribute_name) in enumerate(attributes.keys()):
+                category_names = attributes[attribute_name]
+                count_categories = len(indices_attribute[attribute_name])
+                binary_vector_category = torch.zeros(count_categories)
+
+                if isinstance(category_names, list):
+                    for category_name in category_names:
+                        index_category = indices_attribute[attribute_name][category_name]
+                        binary_vector_category[index_category] = 1
+                else:
+                    index_category = indices_attribute[attribute_name][category_names]
+                    binary_vector_category[index_category] = 1
+
+                dataset[i].append(binary_vector_category)
+
+            count_original = len(indices_original)
+            index_original = indices_original[class_name_original]
+            binary_vector_original = torch.zeros(count_original)
+            binary_vector_original[index_original] = 1
+
+            dataset[-1].append(binary_vector_original)
+
+    for i in range(len(dataset)):
+        dataset[i] = torch.stack(dataset[i], dim = 0)
+        dataset[i] = dataset[i].to(device)
+
+    return dataset
+
+def test(spn_joint, settings):
+    log_likelihoods = spn_joint(settings, False)
+    log_likelihood_test = torch.mean(log_likelihoods).item()
+
+    logger.log_info("Testing log likelihood: " + str(log_likelihood_test) + ".")
+
+    return
 
 def main():
     argument.processArgumentsTestSPN()
 
-    utility.setSeed(header.seed)
+    utility.setSeed(header.config_spn["seed"])
     torch.backends.cuda.matmul.allow_tf32 = header.cuda_allow_tf32
 
-    file_config_dataset = open(header.file_path_dataset_config, "r")
-    config_dataset = json.load(file_config_dataset)
-    file_config_dataset.close()
+    wandb.init(config = header.config_spn, mode = "disabled")
 
     device = torch.device("cuda")
-    spn_joint = spn.SPN()
-    spn_marginal = spn.SPN()
+    dataset_test = loadDataset(header.config_spn["dir_dataset_test"], device)
+    spn_joint = spn.SPN(device)
 
     logger.log_info("Loading SPN from \"" + header.config_spn["file_path_spn"] + "\"...")
 
     spn_joint.load(header.config_spn["file_path_spn"])
-    spn_marginal.load(header.config_spn["file_path_spn"])
-
-    logger.log_info("Loading SPN leaf node settings...")
-
-    spn_settings_joint = utility.generateSPNSettings(config_dataset, device)
-    spn_settings_marginal = torch.clone(spn_settings_joint)
-    spn_settings_marginal[:, -1] = -1
-
-    logger.log_info("Setting SPN leaf nodes...")
-
-    spn_joint.set_leaf_nodes(spn_settings_joint)
-    spn_marginal.set_leaf_nodes(spn_settings_marginal)
-
-    utility.loadCheckpointBestSPN(spn_joint, header.config_spn["dir_checkpoints"], header.config_spn["file_name_checkpoint_best"])
-    utility.loadCheckpointBestSPN(spn_marginal, header.config_spn["dir_checkpoints"], header.config_spn["file_name_checkpoint_best"])
 
     if header.show_model_summary:
         logger.log_info("Number of nodes: " + str(len(spn_joint.nodes)) + ".")
@@ -71,13 +116,9 @@ def main():
         logger.log_info("Number of product nodes: " + str(len(spn_joint.product_nodes)) + ".")
         logger.log_info("Number of leaf nodes: " + str(len(spn_joint.leaf_nodes)) + ".")
         logger.log_info("SPN depths: " + str(spn_joint.depth) + ".")
-        logger.log_info("SPN leaf node setting dimension: (" + str(int(spn_settings_joint.shape[0])) + ", " + str(int(spn_settings_joint.shape[1])) + ").")
 
-    log_likelihoods_joint = spn_joint.forward()
-    log_likelihoods_marginal = spn_marginal.forward()
-
-    logger.log_trace("Testing joint log likelihoods: " + str(log_likelihoods_joint) + ".")
-    logger.log_trace("Testing marginal log likelihoods: " + str(log_likelihoods_marginal) + ".")
+    utility.loadCheckpointBestSPN(spn_joint, header.config_spn["dir_checkpoints"], header.config_spn["file_name_checkpoint_best"])
+    test(spn_joint, dataset_test)
 
     return
 
--- src/train_spn.py
+++ src/train_spn.py
@@ -2,99 +2,14 @@
 
 import argument
 import header
-import json
 import logger
-import os
 import spn
+import test_spn
 import torch
 import tqdm
 import type
 import utility
 import wandb
-
-def loadDataset(dir_dataset, device):
-    config_dataset = {}
-    dataset = []
-
-    with open(header.file_path_dataset_config, "r") as file_config_dataset:
-        config_dataset = json.load(file_config_dataset)
-
-    labels_attribute = utility.getLabelsAttribute(config_dataset)
-    labels_original = utility.getLabelsOriginal(config_dataset)
-    indices_attribute = utility.getIndicesFromLabelsAttribute(labels_attribute)
-    indices_original = utility.getIndicesFromLabelsOriginal(labels_original)
-
-    for _ in range(len(labels_attribute) + 1):
-        dataset.append([])
-
-    if "instance_wise" not in config_dataset or config_dataset["instance_wise"] == False:
-        for class_name_original in labels_original:
-            attributes = config_dataset["mappings"][class_name_original]["labels"]
-            count_instances = len(os.listdir(os.path.join(dir_dataset, class_name_original)))
-
-            for (i, attribute_name) in enumerate(attributes.keys()):
-                category_names = attributes[attribute_name]
-                count_categories = len(indices_attribute[attribute_name])
-                binary_vector_category = torch.zeros(count_categories)
-
-                if isinstance(category_names, list):
-                    for category_name in category_names:
-                        index_category = indices_attribute[attribute_name][category_name]
-                        binary_vector_category[index_category] = 1
-                else:
-                    index_category = indices_attribute[attribute_name][category_names]
-                    binary_vector_category[index_category] = 1
-
-                for _ in range(count_instances):
-                    dataset[i].append(binary_vector_category)
-
-            count_original = len(indices_original)
-            index_original = indices_original[class_name_original]
-            binary_vector_original = torch.zeros(count_original)
-            binary_vector_original[index_original] = 1
-
-            for _ in range(count_instances):
-                dataset[-1].append(binary_vector_original)
-    else:
-        for image_name in config_dataset["mappings"]:
-            class_name_original = image_name.split('/')[0]
-            attributes = config_dataset["mappings"][image_name]["labels"]
-
-            for (i, attribute_name) in enumerate(attributes.keys()):
-                category_names = attributes[attribute_name]
-                count_categories = len(indices_attribute[attribute_name])
-                binary_vector_category = torch.zeros(count_categories)
-
-                if isinstance(category_names, list):
-                    for category_name in category_names:
-                        index_category = indices_attribute[attribute_name][category_name]
-                        binary_vector_category[index_category] = 1
-                else:
-                    index_category = indices_attribute[attribute_name][category_names]
-                    binary_vector_category[index_category] = 1
-
-                dataset[i].append(binary_vector_category)
-
-            count_original = len(indices_original)
-            index_original = indices_original[class_name_original]
-            binary_vector_original = torch.zeros(count_original)
-            binary_vector_original[index_original] = 1
-
-            dataset[-1].append(binary_vector_original)
-
-    for i in range(len(dataset)):
-        dataset[i] = torch.stack(dataset[i], dim = 0)
-        dataset[i] = dataset[i].to(device)
-
-    return dataset
-
-def test(spn_joint, settings):
-    log_likelihoods = spn_joint(settings, False)
-    log_likelihood_test = torch.mean(log_likelihoods).item()
-
-    logger.log_info("Testing log likelihood: " + str(log_likelihood_test) + ".")
-
-    return
 
 def train(spn_joint, spn_marginal, settings_joint, settings_marginal, optimizer, use_probability):
     log_likelihoods = spn_joint(settings_joint, False)
@@ -132,9 +47,9 @@ def main():
     logger.log_info("Started run \"" + header.run_name_spn + "\".")
 
     device = torch.device("cuda")
-    dataset_test = loadDataset(header.config_spn["dir_dataset_test"], device)
-    dataset_train = loadDataset(header.config_spn["dir_dataset_train"], device)
-    dataset_validation = loadDataset(header.config_spn["dir_dataset_validation"], device)
+    dataset_test = test_spn.loadDataset(header.config_spn["dir_dataset_test"], device)
+    dataset_train = test_spn.loadDataset(header.config_spn["dir_dataset_train"], device)
+    dataset_validation = test_spn.loadDataset(header.config_spn["dir_dataset_validation"], device)
     epoch = 1
     log_likelihood_best = float("-inf")
     log_likelihood_train = 0
@@ -184,7 +99,7 @@ def main():
         logger.log_info("SPN depths: " + str(spn_joint.depth) + ".")
 
     logger.log_info("Testing SPN...")
-    test(spn_joint, dataset_test)
+    test_spn.test(spn_joint, dataset_test)
 
     if epoch <= header.config_spn["epochs"]:
         progress_bar = tqdm.tqdm(total = header.config_spn["epochs"], position = 0)
@@ -241,7 +156,7 @@ def main():
         wandb.run.resumed = False
 
     utility.loadCheckpointBestSPN(spn_joint, header.config_spn["dir_checkpoints"], header.config_spn["file_name_checkpoint_best"])
-    test(spn_joint, dataset_test)
+    test_spn.test(spn_joint, dataset_test)
 
     return
 
