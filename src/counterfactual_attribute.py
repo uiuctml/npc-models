961aacae84f4ab93a07bcd536e6ad3818437160f
Simon Yu <jundayu2@illinois.edu>
2025-01-03 18:32:24 -0600

    Add SPN forward on CPU

--- src/counterfactual_attribute.py
+++ src/counterfactual_attribute.py
@@ -543,11 +543,17 @@ def main():
     config_dataset = dataset_test.config
     data_loader_test = torch.utils.data.DataLoader(dataset_test, batch_size = header.config_decomposed["data_loader_batch_size"], shuffle = False, num_workers = header.config_decomposed["data_loader_worker_count"], pin_memory = True)
     device = torch.device("cuda")
+    device_spn = torch.device("cuda")
+
+    if header.composed_spn_on_cpu:
+        logger.log_info("Computing SPNs on CPU.")
+        device_spn = torch.device("cpu")
+
     model_decomposed = model.createModelDecomposed(device)
     model_decomposed = torch.nn.DataParallel(model_decomposed)
     model_decomposed = model_decomposed.to(device)
-    spn_joint = spn.SPN(device)
-    spn_marginal = spn.SPN(device)
+    spn_joint = spn.SPN(device_spn)
+    spn_marginal = spn.SPN(device_spn)
 
     logger.log_info("Loading SPN from \"" + header.config_spn["file_path_spn"] + "\"...")
 
