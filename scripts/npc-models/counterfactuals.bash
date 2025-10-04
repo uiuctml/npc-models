873966301e97869c203408eb5b018512312630bf
Simon Yu <jundayu2@illinois.edu>
2025-10-04 02:01:54 -0500

    Rename CE files

---
+++ scripts/npc-models/counterfactuals.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd "../../src/npc-models"
+
+./counterfactuals.py -r "42.decomposed.resnet34_mtl.2024.12.13.16.41.Aurora-R11" -p "42.spn.pgd.2024.12.13.16.41.Aurora-R11" -s 42
