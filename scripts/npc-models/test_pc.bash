391283c2d7af38b6743e29dd7591d405e6aec97b
Simon Yu <jundayu2@illinois.edu>
2025-10-07 21:33:02 -0500

    Update PC testing bash script

--- scripts/npc-models/test_pc.bash
+++ scripts/npc-models/test_pc.bash
@@ -2,8 +2,26 @@
 
 cd "../../src/npc-models"
 
-./test_pc.py -r "42.awa2.pc.pgd.2024.12.13.16.41.Aurora-R11"
-./test_pc.py -r ""
-./test_pc.py -r ""
-./test_pc.py -r ""
-./test_pc.py -r ""
+./test_pc.py -r "42.awa2.pc.cccp.2024.12.12.19.55.Aurora-R11"
+./test_pc.py -r "52.awa2.pc.cccp.2024.12.22.15.16.Aurora-R11"
+./test_pc.py -r "62.awa2.pc.cccp.2024.12.22.15.17.Aurora-R11"
+./test_pc.py -r "72.awa2.pc.cccp.2024.12.22.15.17.Aurora-R11"
+./test_pc.py -r "82.awa2.pc.cccp.2024.12.22.15.18.Aurora-R11"
+
+./test_pc.py -r "42.celeba.pc.cccp.2024.12.17.21.53.Aurora-R11"
+./test_pc.py -r "52.celeba.pc.cccp.2024.12.22.15.24.Aurora-R11"
+./test_pc.py -r "62.celeba.pc.cccp.2024.12.22.15.26.Aurora-R11"
+./test_pc.py -r "72.celeba.pc.cccp.2024.12.22.15.28.Aurora-R11"
+./test_pc.py -r "82.celeba.pc.cccp.2024.12.22.15.29.Aurora-R11"
+
+./test_pc.py -r "42.gtsrb.pc.cccp.2024.12.11.16.22.Aurora-R11"
+./test_pc.py -r "52.gtsrb.pc.cccp.2024.12.22.15.33.Aurora-R11"
+./test_pc.py -r "62.gtsrb.pc.cccp.2024.12.22.15.33.Aurora-R11"
+./test_pc.py -r "72.gtsrb.pc.cccp.2024.12.22.15.34.Aurora-R11"
+./test_pc.py -r "82.gtsrb.pc.cccp.2024.12.22.15.34.Aurora-R11"
+
+./test_pc.py -r "42.mnist.pc.cccp.2024.12.11.16.23.Aurora-R11"
+./test_pc.py -r "52.mnist.pc.cccp.2024.12.22.15.53.Aurora-R11"
+./test_pc.py -r "62.mnist.pc.cccp.2024.12.22.15.54.Aurora-R11"
+./test_pc.py -r "72.mnist.pc.cccp.2024.12.22.15.55.Aurora-R11"
+./test_pc.py -r "82.mnist.pc.cccp.2024.12.22.15.56.Aurora-R11"
