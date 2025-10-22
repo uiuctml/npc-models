705535cb133327eb34e0a37e295e822b30fee7b0
Simon Yu <jundayu2@illinois.edu>
2025-10-22 13:46:07 -0500

    Update baseline bash scripts

--- scripts/npc-models/train_baseline.bash
+++ scripts/npc-models/train_baseline.bash
@@ -13,23 +13,44 @@ cd "$(dirname $0)"
 # Go to source directory
 cd "../../src/npc-models"
 
-./train_baseline.py -m "abm" -b 256 -e 150 -s 42
-./train_baseline.py -m "abm" -b 256 -e 150 -s 52
-./train_baseline.py -m "abm" -b 256 -e 150 -s 62
-./train_baseline.py -m "abm" -b 256 -e 150 -s 72
-./train_baseline.py -m "abm" -b 256 -e 150 -s 82
-./train_baseline.py -m "cbm" -b 256 -e 150 -s 42
-./train_baseline.py -m "cbm" -b 256 -e 150 -s 52
-./train_baseline.py -m "cbm" -b 256 -e 150 -s 62
-./train_baseline.py -m "cbm" -b 256 -e 150 -s 72
-./train_baseline.py -m "cbm" -b 256 -e 150 -s 82
-./train_baseline.py -m "cem" -b 256 -e 150 -s 42
-./train_baseline.py -m "cem" -b 256 -e 150 -s 52
-./train_baseline.py -m "cem" -b 256 -e 150 -s 62
-./train_baseline.py -m "cem" -b 256 -e 150 -s 72
-./train_baseline.py -m "cem" -b 256 -e 150 -s 82
-./train_baseline.py -m "dcr" -b 256 -e 150 -s 42
-./train_baseline.py -m "dcr" -b 256 -e 150 -s 52
-./train_baseline.py -m "dcr" -b 256 -e 150 -s 62
-./train_baseline.py -m "dcr" -b 256 -e 150 -s 72
-./train_baseline.py -m "dcr" -b 256 -e 150 -s 82
+# Attribute Bottleneck Model (ABM)
+./train_baseline.py -m "abm" -c 0 -w 1 -b 256 -e 150 -s 42
+./train_baseline.py -m "abm" -c 0 -w 1 -b 256 -e 150 -s 52
+./train_baseline.py -m "abm" -c 0 -w 1 -b 256 -e 150 -s 62
+./train_baseline.py -m "abm" -c 0 -w 1 -b 256 -e 150 -s 72
+./train_baseline.py -m "abm" -c 0 -w 1 -b 256 -e 150 -s 82
+
+# Concept Bottleneck Model (CBM) [Original]
+./train_baseline.py -m "cbm" -c 0 -w 1 -b 256 -e 150 -s 42
+./train_baseline.py -m "cbm" -c 0 -w 1 -b 256 -e 150 -s 52
+./train_baseline.py -m "cbm" -c 0 -w 1 -b 256 -e 150 -s 62
+./train_baseline.py -m "cbm" -c 0 -w 1 -b 256 -e 150 -s 72
+./train_baseline.py -m "cbm" -c 0 -w 1 -b 256 -e 150 -s 82
+
+# Concept Bottleneck Model (CBM) [Hybrid, Supervised]
+./train_baseline.py -m "cbm" -c 1 -w 1 -b 256 -e 150 -s 42
+./train_baseline.py -m "cbm" -c 1 -w 1 -b 256 -e 150 -s 52
+./train_baseline.py -m "cbm" -c 1 -w 1 -b 256 -e 150 -s 62
+./train_baseline.py -m "cbm" -c 1 -w 1 -b 256 -e 150 -s 72
+./train_baseline.py -m "cbm" -c 1 -w 1 -b 256 -e 150 -s 82
+
+# Concept Bottleneck Model (CBM) [Hybrid, Unsupervised]
+./train_baseline.py -m "cbm" -c 1 -w 0 -b 256 -e 150 -s 42
+./train_baseline.py -m "cbm" -c 1 -w 0 -b 256 -e 150 -s 52
+./train_baseline.py -m "cbm" -c 1 -w 0 -b 256 -e 150 -s 62
+./train_baseline.py -m "cbm" -c 1 -w 0 -b 256 -e 150 -s 72
+./train_baseline.py -m "cbm" -c 1 -w 0 -b 256 -e 150 -s 82
+
+# Concept Embedding Model (CEM)
+./train_baseline.py -m "cem" -c 0 -w 1 -b 256 -e 150 -s 42
+./train_baseline.py -m "cem" -c 0 -w 1 -b 256 -e 150 -s 52
+./train_baseline.py -m "cem" -c 0 -w 1 -b 256 -e 150 -s 62
+./train_baseline.py -m "cem" -c 0 -w 1 -b 256 -e 150 -s 72
+./train_baseline.py -m "cem" -c 0 -w 1 -b 256 -e 150 -s 82
+
+# Deep Concept Reasoner (DCR)
+./train_baseline.py -m "dcr" -c 0 -w 1 -b 256 -e 150 -s 42
+./train_baseline.py -m "dcr" -c 0 -w 1 -b 256 -e 150 -s 52
+./train_baseline.py -m "dcr" -c 0 -w 1 -b 256 -e 150 -s 62
+./train_baseline.py -m "dcr" -c 0 -w 1 -b 256 -e 150 -s 72
+./train_baseline.py -m "dcr" -c 0 -w 1 -b 256 -e 150 -s 82
