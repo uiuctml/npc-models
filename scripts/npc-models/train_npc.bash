6d87aaf230e792667184f97230718e09fc255749
Simon Yu <jundayu2@illinois.edu>
2025-10-30 22:24:10 -0500

    Add random PC placeholders

--- scripts/npc-models/train_npc.bash
+++ scripts/npc-models/train_npc.bash
@@ -56,3 +56,25 @@ cd "../../src/npc-models"
 ./train_npc.py -w "62.mnist.neural.resnet34mtl.2024.12.24.4.40.PowerEdge-R720.best.zip" -b 256 -e 150 -s 62
 ./train_npc.py -w "72.mnist.neural.resnet34mtl.2024.12.24.9.9.PowerEdge-R720.best.zip" -b 256 -e 150 -s 72
 ./train_npc.py -w "82.mnist.neural.resnet34mtl.2024.12.24.13.37.PowerEdge-R720.best.zip" -b 256 -e 150 -s 82
+
+# Joint optimization with random PC
+./train_npc.py -w "42.awa2.neural.resnet34mtl.2024.12.12.20.5.Aurora-R11.best.zip" -c ".best.zip" -b 256 -e 150 -s 42
+./train_npc.py -w "52.awa2.neural.resnet34mtl.2024.12.24.0.30.Aurora-R11.best.zip" -c ".best.zip" -b 256 -e 150 -s 52
+./train_npc.py -w "62.awa2.neural.resnet34mtl.2024.12.24.3.25.Aurora-R11.best.zip" -c ".best.zip" -b 256 -e 150 -s 62
+./train_npc.py -w "72.awa2.neural.resnet34mtl.2024.12.24.6.21.Aurora-R11.best.zip" -c ".best.zip" -b 256 -e 150 -s 72
+./train_npc.py -w "82.awa2.neural.resnet34mtl.2024.12.24.9.16.Aurora-R11.best.zip" -c ".best.zip" -b 256 -e 150 -s 82
+./train_npc.py -w "42.celeba.neural.resnet34mtl.2024.12.17.21.56.Aurora-R11.best.zip" -c ".best.zip" -b 256 -e 150 -s 42
+./train_npc.py -w "52.celeba.neural.resnet34mtl.2024.12.26.2.57.Fractal-Define-XL-R2.best.zip" -c ".best.zip" -b 256 -e 150 -s 52
+./train_npc.py -w "62.celeba.neural.resnet34mtl.2024.12.26.19.16.Fractal-Define-XL-R2.best.zip" -c ".best.zip" -b 256 -e 150 -s 62
+./train_npc.py -w "72.celeba.neural.resnet34mtl.2024.12.26.2.58.Fractal-Define-XL-R2.best.zip" -c ".best.zip" -b 256 -e 150 -s 72
+./train_npc.py -w "82.celeba.neural.resnet34mtl.2024.12.26.19.20.Fractal-Define-XL-R2.best.zip" -c ".best.zip" -b 256 -e 150 -s 82
+./train_npc.py -w "42.gtsrb.neural.resnet34mtl.2024.12.10.18.10.PowerEdge-R720.best.zip" -c ".best.zip" -b 256 -e 150 -s 42
+./train_npc.py -w "52.gtsrb.neural.resnet34mtl.2024.12.24.17.41.Aurora-R11.best.zip" -c ".best.zip" -b 256 -e 150 -s 52
+./train_npc.py -w "62.gtsrb.neural.resnet34mtl.2024.12.24.5.54.PowerEdge-R720.best.zip" -c ".best.zip" -b 256 -e 150 -s 62
+./train_npc.py -w "72.gtsrb.neural.resnet34mtl.2024.12.24.11.37.PowerEdge-R720.best.zip" -c ".best.zip" -b 256 -e 150 -s 72
+./train_npc.py -w "82.gtsrb.neural.resnet34mtl.2024.12.24.17.21.PowerEdge-R720.best.zip" -c ".best.zip" -b 256 -e 150 -s 82
+./train_npc.py -w "42.mnist.neural.resnet34mtl.2024.12.10.18.11.PowerEdge-R720.best.zip" -c ".best.zip" -b 256 -e 150 -s 42
+./train_npc.py -w "52.mnist.neural.resnet34mtl.2024.12.24.0.10.PowerEdge-R720.best.zip" -c ".best.zip" -b 256 -e 150 -s 52
+./train_npc.py -w "62.mnist.neural.resnet34mtl.2024.12.24.4.40.PowerEdge-R720.best.zip" -c ".best.zip" -b 256 -e 150 -s 62
+./train_npc.py -w "72.mnist.neural.resnet34mtl.2024.12.24.9.9.PowerEdge-R720.best.zip" -c ".best.zip" -b 256 -e 150 -s 72
+./train_npc.py -w "82.mnist.neural.resnet34mtl.2024.12.24.13.37.PowerEdge-R720.best.zip" -c ".best.zip" -b 256 -e 150 -s 82
--- scripts/npc-models/train_pc.bash
+++ scripts/npc-models/train_pc.bash
@@ -13,8 +13,16 @@ cd "$(dirname $0)"
 # Go to source directory
 cd "../../src/npc-models"
 
+# Data-driven PC
 ./train_pc.py -e 50 -s 42
 ./train_pc.py -e 50 -s 52
 ./train_pc.py -e 50 -s 62
 ./train_pc.py -e 50 -s 72
 ./train_pc.py -e 50 -s 82
+
+# Random PC
+./train_pc.py -e 200 -s 42
+./train_pc.py -e 200 -s 52
+./train_pc.py -e 200 -s 62
+./train_pc.py -e 200 -s 72
+./train_pc.py -e 200 -s 82
